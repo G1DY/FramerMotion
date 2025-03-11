58d3247884d1ea05c224148dfd0f0f7374e05e94
gideon <kipkerring.gedion@gmail.com>
2025-03-11 17:42:11 +0300

    testing tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 const App = () => {
   return (
     <div className="p-4 border-lg justify-center flex-col flex items-center min-h-screen ">
-      <div className="border rounded justify-center flex shadow-lg bg-teal-400 hover:bg-teal-600">
-        <h1 className="text-3xl text-black">
+      <div className="border rounded-lg justify-center flex shadow-lg bg-teal-400 hover:bg-teal-600">
+        <h1 className="text-3xl text-black text-center px-2 py-4">
           Hello We are learning Framer Motion
         </h1>
       </div>
