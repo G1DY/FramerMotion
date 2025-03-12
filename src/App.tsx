7bbcce5c65fe75a22ff318f33d358b0686e1122d
gideon <kipkerring.gedion@gmail.com>
2025-03-12 11:31:30 +0300

    Learning variants with a flippingcard project

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import FlippingCard from "./FlippingCard";
 
 const App = () => {
   return (
-    <div>
+    <div className="min-h-screen flex items-center justify-center bg-gray-100">
       {/* <PulsingButton /> */}
       {/* <BouncingLoader /> */}
       <FlippingCard />
