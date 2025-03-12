f52ede364d82a3762323b604f3926b6bf6ebabea
gideon <kipkerring.gedion@gmail.com>
2025-03-12 08:07:50 +0300

    understanding framer motions, using initial props, animate and exit

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import Accordian from "./Accordian";
+import { motion } from "framer-motion";
 
 const App = () => {
   return (
-    <div>
-      {/* <div className="border rounded-lg shadow-lg bg-teal-400 hover:bg-teal-600">
-        <h1 className="text-3xl text-black text-center px-2 py-4">
-          Hello We are learning Framer Motion
-        </h1>
-      </div> */}
-      <Accordian />
-    </div>
+    <motion.div
+      className="box justify-center flex items-center m-20px"
+      animate={{ translateX: 200 }}
+    ></motion.div>
   );
 };
 export default App;
