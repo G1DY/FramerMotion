2ca357bcd76ae982208f9b12b3154ef292140ab1
gideon <kipkerring.gedion@gmail.com>
2025-03-12 10:00:30 +0300

    pulsing button project

--- src/PulsingButton.tsx
+++ src/PulsingButton.tsx
@@ -5,7 +5,10 @@ const PulsingButton = () => {
     <div>
       <motion.button
         className="bg-blue-400 px-8 py-4 outline-none border-none rounded-lg "
-        animate={{ scale: [1, 2, 3, 2, 1] }}
+        animate={{
+          scale: [1, 2, 3, 2, 1],
+          backgroundColor: ["#3b82f6", "#60a5fa", "#3b82f6"],
+        }}
         transition={{ repeat: Infinity, duration: 0.8, ease: "easeInOut" }}
       >
         click me
