4c04cd140cb9d113e2f5495af953c622a8b88c52
gideon <kipkerring.gedion@gmail.com>
2025-03-13 16:42:11 +0300

    Toaster project

--- src/Transformations/Toaster.tsx
+++ src/Transformations/Toaster.tsx
@@ -18,7 +18,13 @@ const Toaster = () => {
       </button>
       <AnimatePresence>
         {visible && (
-          <motion.div className="fixed top-4 right-4 p-4 bg-green-400 text-white rounded">
+          <motion.div
+            className="fixed top-4 right-4 p-4 bg-green-400 text-white rounded"
+            initial={{ opacity: 0, translateY: -20 }}
+            animate={{ opacity: 1, translateY: 0 }}
+            exit={{ opacity: 0, translateY: -20 }}
+            transition={{ duration: 0.5 }}
+          >
             Notification: Action Successful
           </motion.div>
         )}
