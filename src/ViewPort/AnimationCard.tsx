c81d0b7ba56361152fcdb33136843fef55859e74
gideon <kipkerring.gedion@gmail.com>
2025-03-13 09:51:00 +0300

    creating a scroll framer motion and appling whileView prop

--- src/ViewPort/AnimationCard.tsx
+++ src/ViewPort/AnimationCard.tsx
@@ -1,11 +1,20 @@
+import { motion } from "framer-motion";
 const AnimationCard = () => {
   return (
     <div className=" items-center ml-[40%] justify-center flex mt-[50rem]">
       <div className="h-[200vh] w-full justify-center flex items-center text-center">
-        <div className="bg-blue-400 rounded-lg p-6 shadow-lg text-center">
+        <motion.div
+          className="bg-blue-400 rounded-lg p-6 shadow-lg text-center"
+          initial={{ scale: 0, opacity: 0 }}
+          whileInView={{
+            scale: 1.1,
+            opacity: 1,
+          }}
+          transition={{ duration: 0.8 }}
+        >
           <h2 className="text-2xl font-bold text-gray-900">AnimatedCard</h2>
           <p className="text-sm">This is the animated card</p>
-        </div>
+        </motion.div>
       </div>
     </div>
   );
