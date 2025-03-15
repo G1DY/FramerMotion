babac03dc3260afb1e3b9eb89eb23575b8bb237a
gideon <kipkerring.gedion@gmail.com>
2025-03-15 13:45:45 +0300

    This is the setup for all projects done with framer motion

---
+++ FramerMotionProjects/src/App.tsx
@@ -0,0 +1,8 @@
+const App = () => {
+  return (
+    <div className="text-2xl font-bold justify-center flex h-screen items-center">
+      Welcome to Practical framer motion projects
+    </div>
+  );
+};
+export default App;
