5997365ad5ecbd1948fbb355b17ebfc18df1b9a5
gideon <kipkerring.gedion@gmail.com>
2025-03-15 14:34:53 +0300

    scroll indicator project

--- FramerMotionProjects/src/App.tsx
+++ FramerMotionProjects/src/App.tsx
@@ -2,7 +2,7 @@ import ScrollIndicator from "./ScrollIndicator";
 
 const App = () => {
   return (
-    <div className="justify-center flex h-[200rem] p-[20px] items-center">
+    <div className="justify-center flex h-auto items-center">
       <ScrollIndicator />
     </div>
   );
--- FramerMotionProjects/src/ScrollIndicator.tsx
+++ FramerMotionProjects/src/ScrollIndicator.tsx
@@ -4,14 +4,23 @@ const ScrollIndicator = () => {
   const { scrollYProgress } = useScroll();
 
   const lineWidth = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
+  const textOpacity = useTransform(scrollYProgress, [0, 0.1], [0, 1]);
+  const textTranslateY = useTransform(
+    scrollYProgress,
+    [0, 1],
+    ["50px", "100px"]
+  );
   return (
     <>
       <motion.div
-        className="fixed left-0 top-0 bg-red-500 h-[20px]"
+        className="fixed left-0 top-0 bg-red-500 h-[20px] overflow-hidden"
         style={{ width: lineWidth, transition: "width 0.2s ease" }}
       />
-      <div className="mt-[50px] relative">
-        {[...Array(20)].map((i) => (
+      <motion.div
+        className="mt-[10px] relative p-4"
+        style={{ y: textTranslateY, opacity: textOpacity }}
+      >
+        {[...Array(100)].map((i) => (
           <p key={i}>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam
             placeat recusandae in nisi excepturi dignissimos cum quisquam vero
@@ -19,7 +28,7 @@ const ScrollIndicator = () => {
             quo. Iste, ipsa?
           </p>
         ))}
-      </div>
+      </motion.div>
     </>
   );
 };
