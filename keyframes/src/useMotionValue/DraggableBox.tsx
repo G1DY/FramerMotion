9f1d0c9ac22fdfffc5856b8db9418d692f6dc6c2
gideon <kipkerring.gedion@gmail.com>
2025-03-15 11:07:11 +0300

    draggable box projects using useMotionValue

--- keyframes/src/useMotionValue/DraggableBox.tsx
+++ keyframes/src/useMotionValue/DraggableBox.tsx
@@ -1,4 +1,16 @@
+import { motion, useMotionValue } from "framer-motion";
+
 const DraggableBox = () => {
-  return <div>DraggableBox</div>;
+  const x = useMotionValue(0);
+  const y = useMotionValue(0);
+  return (
+    <motion.div className="flex flex-col">
+      <motion.div style={{ x, y }} drag className="w-32 h-32 bg-blue-400 " />
+
+      <p>
+        Position({x.get().toFixed(0)}, {y.get().toFixed(0)})
+      </p>
+    </motion.div>
+  );
 };
 export default DraggableBox;
