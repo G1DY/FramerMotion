9024aa99506724c002fe31fce262096e54964612
gideon <kipkerring.gedion@gmail.com>
2025-03-17 18:58:27 +0300

    loader project

--- FramerMotionProjects/src/RotatingButton.tsx
+++ FramerMotionProjects/src/RotatingButton.tsx
@@ -1,8 +1,10 @@
+import { motion } from "framer-motion";
+
 const RotatingButton = () => {
   return (
-    <div className="relative  rounded-full border-4 border-top-4 p-6">
-      <div className="absolute rounded-full border-4 border-top-4 items-center justify-center"></div>
-    </div>
+    <motion.div className="relative  rounded-full border-20 border-top-4 p-6 justify-center border-blue-800 flex items-center">
+      <motion.div className="absolute rounded-full border-8 border-top-4 items-center justify-center border-blue-500 p-7"></motion.div>
+    </motion.div>
   );
 };
 export default RotatingButton;
