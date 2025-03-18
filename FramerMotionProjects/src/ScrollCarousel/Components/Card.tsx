9fdd68ced9b34eae16e0150d6e5d2e8cb11041c8
gideon <kipkerring.gedion@gmail.com>
2025-03-18 17:17:44 +0300

    scrollcarousel project

--- FramerMotionProjects/src/ScrollCarousel/Components/Card.tsx
+++ FramerMotionProjects/src/ScrollCarousel/Components/Card.tsx
@@ -1,4 +1,18 @@
-const Card = () => {
-  return <div>Card</div>;
+const Card = ({ card, idx }: { card: any; idx: number }) => {
+  return (
+    <div
+      key={idx}
+      className="group relative w-[10rem] h-[15rem] m-2 overflow-hidden bg-neutral-700"
+    >
+      <div
+        style={{
+          backgroundImage: `url(${card.url})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+        }}
+        className="absolute inset-0 z-0 transition-transform duration-300 group-hover:scale-110"
+      ></div>
+    </div>
+  );
 };
 export default Card;
