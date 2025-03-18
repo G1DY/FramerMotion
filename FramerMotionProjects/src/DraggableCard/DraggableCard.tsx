9a6643f179d9dcda10b2fa807976d8ff638e7477
gideon <kipkerring.gedion@gmail.com>
2025-03-18 08:48:34 +0300

    draggable card project

--- FramerMotionProjects/src/DraggableCard/DraggableCard.tsx
+++ FramerMotionProjects/src/DraggableCard/DraggableCard.tsx
@@ -15,6 +15,7 @@ const DraggableCard = ({ children, style }: componentProps) => {
       dragConstraints={{ left: -200, right: 200, top: -200, bottom: 200 }}
       whileHover={{ scale: 1.1 }}
       whileTap={{ scale: 0.9 }}
+      transition={{ duration: 0.5 }}
     >
       {children}
     </motion.div>
