21ad8735ee280c2d6a3b9c89572bdece1c017042
gideon <kipkerring.gedion@gmail.com>
2025-03-18 08:32:09 +0300

    draggable card project

--- FramerMotionProjects/src/DraggableCard/DraggableCard.tsx
+++ FramerMotionProjects/src/DraggableCard/DraggableCard.tsx
@@ -9,10 +9,12 @@ interface componentProps {
 const DraggableCard = ({ children, style }: componentProps) => {
   return (
     <motion.div
-      className="w-52 h-72 m-2 p-5 rounded-xl "
+      className="w-52 h-72 m-2 p-5 rounded-xl justify-center flex items-center text-white text-2xl "
       style={style}
       drag
       dragConstraints={{ left: -200, right: 200, top: -200, bottom: 200 }}
+      whileHover={{ scale: 1.1 }}
+      whileTap={{ scale: 0.9 }}
     >
       {children}
     </motion.div>
