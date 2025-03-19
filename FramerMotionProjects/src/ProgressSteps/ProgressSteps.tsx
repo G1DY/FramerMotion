f1780d98b73e93e9d11961ce8986f7d07126b838
gideon <kipkerring.gedion@gmail.com>
2025-03-19 09:01:49 +0300

    ProgressSteps Project

--- FramerMotionProjects/src/ProgressSteps/ProgressSteps.tsx
+++ FramerMotionProjects/src/ProgressSteps/ProgressSteps.tsx
@@ -10,6 +10,38 @@ const steps = [
 
 const ProgressSteps = () => {
   const [currentStep, setCurrentStep] = useState(0);
-  return <div className="flex flex-col items-center p-5"></div>;
+  return (
+    <div className="flex flex-col items-center p-5">
+      <div className="relative flex justify-between mb-5 w-[40rem]">
+        <motion.div
+          className="absolute h-1 bg-blue-500 mt-[1rem]"
+          style={{
+            width: `${((currentStep + 0) / steps.length) * 130}%`,
+            transition: "width 0.3s ease",
+          }}
+        />
+        {steps.map((step, index) => (
+          <div key={index} className="relative flex flex-col items-center">
+            <motion.div
+              className={`h-10 w-10 flex justify-center items-center rounded-full border-2 ${
+                index <= currentStep ? "bg-blue-500" : "bg-gray-200"
+              }`}
+              whileHover={{ scale: 1.1 }}
+              transition={{ duration: 0.2 }}
+            >
+              {index < currentStep ? "✅" : null}
+            </motion.div>
+            <p
+              className={`mt-2 ${
+                index <= currentStep ? "text-blue-500" : "text-gray-500"
+              }`}
+            >
+              {step.title}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 };
 export default ProgressSteps;
