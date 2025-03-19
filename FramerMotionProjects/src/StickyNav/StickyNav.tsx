f4744e4be1285c40cb401511d23ef8ec4e2a013d
gideon <kipkerring.gedion@gmail.com>
2025-03-19 07:47:49 +0300

    Sticky Navbar Project

--- FramerMotionProjects/src/StickyNav/StickyNav.tsx
+++ FramerMotionProjects/src/StickyNav/StickyNav.tsx
@@ -15,13 +15,40 @@ const StickyNav = () => {
     }),
   };
   return (
-    <div className="bg-blck min-h-screen">
+    <div className="bg-black min-h-screen">
       <motion.div
         className="fixed top-0 w-full p-8 bg-white text-black rounded-lg shadow-lg"
         variants={menuvariants}
         initial="hidden"
         whileHover="visible"
-      ></motion.div>
+      >
+        <div className="flex flex-col">
+          <div className="flex justify-between items-center">
+            <div className="flex items-center">
+              <div className="ml-2 font-semibold uppercase">Gideon</div>
+            </div>
+            <div className="hidden text-center space-x-5 md:flex">
+              <a href="#" className="text-black font-bold">
+                Products
+              </a>
+              <a href="#" className="text-black font-bold">
+                Use Cases
+              </a>
+              <a href="#" className="text-black font-bold">
+                Pricing
+              </a>
+              <a href="#" className="text-black font-bold">
+                Resources
+              </a>
+            </div>
+            <div className="items-center flex">
+              <a href="#" className="text-black font-bold">
+                Join
+              </a>
+            </div>
+          </div>
+        </div>
+      </motion.div>
     </div>
   );
 };
